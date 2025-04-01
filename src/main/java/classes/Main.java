85c4a40c3bda4a1487178fc610839c28b01a164e
Vinicius Durante Bagio <viniciusduranteb@gmail.com>
2025-04-01 20:27:15 -0300

    Criando a classe Main

---
+++ src/main/java/classes/Main.java
@@ -0,0 +1,8 @@
+package classes;
+
+public class Main {
+    // aqui vai a logica do menu interativo, pode ser feito com joptionpane ou direto no console
+    public static void main(String[] args) {
+
+    }
+}
